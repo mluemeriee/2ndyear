d39a5721b5c96d78ea0f7768af06bc81d8a99ef9
mluemeriee <sapatanshanylann@gmail.com>
2026-03-11 10:41:29 +0800

    Add files via upload

---
+++ 1sem/1.php
@@ -0,0 +1,34 @@
+<?php
+
+class Student{
+    //properties - declared variables
+    public $fullname;
+    public $course;
+    public $level;
+    public $idnumber;
+
+    //methods - declared functions
+    public function __construct($fullname, $course, $level, $idnumber){
+        $this->fullname = $fullname;
+        $this->course = $course;
+        $this->level = $level;
+        $this->idnumber = $idnumber;
+    }
+
+    public function introduction(){
+        return "I am $this->fullname a $this->level $this->course student";
+    }
+    public function registration(){
+        return "Congratulations $this->fullname, you have been registered.";
+    }
+    public function status(){
+        return "I am $this->fullname a $this->level $this->course student and my id number is $this->idnumber";
+    }    
+}
+
+$student1= new Student("Sapatan", "BSIT", "2nd year", "24-3802");
+echo $student1->introduction() . "<br>";
+echo $student1->registration() ."<br>";
+echo $student1->status(); ""
+
+?>
